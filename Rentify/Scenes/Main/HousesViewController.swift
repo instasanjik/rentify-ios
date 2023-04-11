e0b5d5e548dafa8aa710984745e84d33542591c1
instasanjik <koshkarbayev.07@gmail.com>
2023-04-12 04:27:08 +0600

    feat (scenes): add table view to houses screen

--- Rentify/Scenes/Main/HousesViewController.swift
+++ Rentify/Scenes/Main/HousesViewController.swift
@@ -6,24 +6,52 @@
 //
 
 import UIKit
+import SnapKit
 
 class HousesViewController: UIViewController {
+    
+    lazy var mainTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = self
+        tableView.delegate = self
+        tableView.backgroundColor = .red
+//        tableView.register(1, forCellReuseIdentifier: "MainTableViewCell")
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        setupMainTableView()
         // Do any additional setup after loading the view.
     }
+
+}
+
+extension HousesViewController {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    fileprivate func setupMainTableView() {
+        view.addSubview(mainTableView)
+        
+        mainTableView.snp.makeConstraints { make in
+            make.left.right.equalToSuperview().inset(24)
+            make.top.equalTo(view.snp.topMargin).inset(24)
+            make.bottom.equalToSuperview()
+        }
     }
-    */
+    
+    
+}
 
+extension HousesViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell(frame: .zero)
+    }
+    
+    
 }
